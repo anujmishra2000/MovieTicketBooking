f0858d5f2da9e50478a6027a401747e127a8bf9f
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-06-05 15:08:14 +0530

    CR done(updated)

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_083823) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
