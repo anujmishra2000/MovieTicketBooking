3878fe54cdb4fe7466d6c51147043d8e0b35adf7
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-06-24 19:20:50 +0530

    CR done and confirmed mail to user

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,6 @@
 class Payment < ApplicationRecord
   include Numberable
+  set_number(letter:'P', length: 6)
 
   belongs_to :order
 
@@ -7,15 +8,10 @@ class Payment < ApplicationRecord
   validates :number, presence: true, uniqueness: true
   validates :amount, numericality: { greater_than_or_equal_to: 0, message: 'must be a valid decimal number' }, allow_blank: true
 
-  before_validation :set_number
-
   enum status: {
     'pending': 0,
     'success': 1,
     'failed': 2
   }
 
-  private def set_number
-    generate_number('P', 8)
-  end
 end
