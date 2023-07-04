1521f739c45d587e1b92e15f4a8bd254e5cf1b41
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-07-04 08:47:43 +0530

    Seats Available CR done

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,27 +2,28 @@ class PaymentsController < ApplicationController
   before_action :set_payment, only: [:success, :cancel, :refund]
 
   def create
+    order = current_user.orders.in_progress.last
     begin
-      order = current_user.orders.in_progress.last
       payment = StripeService.create_stripe_charge(order, params[:stripeToken])
-      Stripe::Charge.capture(payment.charge_id)
-      line_item = order.line_items.first
-      begin
-        line_item.show.with_lock do
-          raise SeatsUnavailableException if line_item.quantity > line_item.show.seats_available
-          line_item.show.seats_available -= line_item.quantity
-          payment.mark_as_success
-          payment.order.mark_as_completed
-          line_item.show.save
-        end
-        redirect_to success_payment_path(payment)
-      rescue SeatsUnavailableException
-        payment.failed!
-        redirect_to root_path, alert: t('.seats_unavailable')
-      end
     rescue Stripe::StripeError => e
       current_user.orders.in_progress.first.payments.last.failed!
-      redirect_to request.referrer, alert: t('.payment_error', error: e.message)
+      return redirect_to cart_order_path(order), alert: t('.payment_error', error: e.message)
+    end
+      line_item = order.line_items.first
+      show = line_item.show
+    begin
+      show.with_lock do
+        raise SeatsUnavailableException if line_item.quantity > show.seats_available
+        Stripe::Charge.capture(payment.charge_id)
+        show.seats_available -= line_item.quantity
+        payment.mark_as_success
+        payment.order.mark_as_completed
+        show.save
+      end
+      redirect_to success_payment_path(payment)
+    rescue SeatsUnavailableException
+      payment.failed!
+      redirect_to root_path, alert: t('.seats_unavailable')
     end
   end
 
