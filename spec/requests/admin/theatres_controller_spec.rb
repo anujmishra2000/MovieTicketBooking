4258e21b3240dec2318df84702ff2e2040f9ec82
Anuj Mishra <anuj.2000.mishra@gmail.com>
2023-07-26 18:31:49 +0530

    Updated Admin::TheatresController spec

--- spec/requests/admin/theatres_controller_spec.rb
+++ spec/requests/admin/theatres_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Admin::TheatresController, type: :request do
-  let!(:user) { create(:user, :admin) }
+  let!(:admin_user) { create(:user, :admin) }
   let!(:country) { create(:country) }
   let!(:theatre) { create(:theatre) }
   let!(:address) { create(:address, country: country, theatre: theatre)}
@@ -24,51 +24,74 @@ RSpec.describe Admin::TheatresController, type: :request do
       }
     }
   end
+  let(:invalid_params) do
+    {
+      theatre: {
+        name: '',
+        screen_type: '',
+        seating_capacity: nil,
+        operational: true,
+        contact_number: '',
+        contact_email: '',
+      }
+    }
+  end
 
-  before { sign_in user }
+  before { sign_in admin_user }
 
   describe "GET #index" do
-    it "returns a successful response" do
-      get admin_theatres_path
+    before { get admin_theatres_path }
+
+    it "should returns a successful response" do
       expect(response).to have_http_status(:success)
     end
 
-    it "displays the correct theatres data" do
+    it "should display the correct theatres data" do
       theatres = create_list(:theatre, 3)
-      get admin_theatres_path
       expect(assigns(:theatres).count).to be(4)
     end
   end
 
   describe "GET #show" do
-    it "returns a successful response" do
-      get admin_theatre_path(theatre)
-      expect(response).to have_http_status(:success)
+    context "with valid theatre id" do
+      it "should returns a successful response" do
+        get admin_theatre_path(theatre)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "with invalid theatre id" do
+      it "should redirects to theatre index with alert message" do
+        invalid_theatre = theatre.id + 1
+        get admin_theatre_path(invalid_theatre)
+        expect(response).to redirect_to(admin_theatres_path)
+        expect(flash[:alert]).to eq('Theatre does not exit')
+      end
     end
   end
 
   describe "GET #new" do
-    it "returns http success" do
-      get new_admin_theatre_path
+    before { get new_admin_theatre_path }
+
+    it "should returns http success" do
       expect(response).to have_http_status(:success)
     end
 
-    it "assigns a new theatre as @theatre" do
-      get new_admin_theatre_path
+    it "should assigns a new theatre as @theatre" do
       expect(assigns(:theatre)).to be_a_new(Theatre)
     end
   end
 
   describe "POST #create" do
     context "with valid params" do
-      it "creates a new theatre" do
+      it "should creates a new theatre" do
         expect {
           post admin_theatres_path, params: valid_params
         }.to change(Theatre, :count).by(1)
         expect(flash[:notice]).to eq('Theatre was successfully created')
       end
 
-      it "redirects to the created theatre" do
+      it "should redirects to the created theatre" do
         post admin_theatres_path, params: valid_params
         expect(response).to redirect_to(admin_theatre_path(Theatre.last))
         expect(flash[:notice]).to eq('Theatre was successfully created')
@@ -76,25 +99,32 @@ RSpec.describe Admin::TheatresController, type: :request do
     end
 
     context "with invalid params" do
-      it "does not create a new theatre" do
+      it "should not create a new theatre" do
         expect {
           post admin_theatres_path, params: { theatre: { name: '' } }
         }.not_to change(Theatre, :count)
       end
 
-      it "renders the 'new' template" do
+      it "should render the 'new' template" do
         post admin_theatres_path, params: { theatre: { name: '' } }
         expect(response).to render_template(:new)
-        expect(flash[:error]).to include("Name can't be blank", "Screen type can't be blank", "Seating capacity can't be blank",
-        "Contact number can't be blank",
-        "Contact email can't be blank")
+      end
+
+      it 'should display error messages' do
+        post admin_theatres_path, params: { theatre: { name: '' } }
+        expect(flash[:error]).to include("Name can't be blank")
+        expect(flash[:error]).to include("Screen type can't be blank")
+        expect(flash[:error]).to include("Seating capacity can't be blank")
+        expect(flash[:error]).to include("Contact number can't be blank")
+        expect(flash[:error]).to include("Contact email can't be blank")
       end
     end
   end
 
   describe "GET #edit" do
-    it "returns http success" do
-      get edit_admin_theatre_path(theatre)
+    before { get edit_admin_theatre_path(theatre) }
+
+    it "should returns http success" do
       expect(response).to have_http_status(:success)
     end
   end
@@ -108,42 +138,47 @@ RSpec.describe Admin::TheatresController, type: :request do
         }
       end
 
-      it "updates the requested theatre" do
-        patch admin_theatre_path(theatre), params: { theatre: new_attributes }
+      before { patch admin_theatre_path(theatre), params: { theatre: new_attributes } }
+
+      it "should updates the requested theatre" do
         theatre.reload
         expect(theatre.name).to eq('Updated Theatre')
         expect(theatre.seating_capacity).to eq(300)
         expect(flash[:notice]).to eq('Theatre was successfully updated.')
       end
 
-      it "redirects to the theatre" do
-        patch admin_theatre_path(theatre), params: { theatre: new_attributes }
+      it "should redirects to the theatre" do
         expect(response).to redirect_to(admin_theatre_path(theatre))
         expect(flash[:notice]).to eq('Theatre was successfully updated.')
       end
     end
 
     context "with invalid params" do
-      it "renders the 'edit' template" do
-        patch admin_theatre_path(theatre), params: { theatre: { name: '' } }
+      before { patch admin_theatre_path(theatre), params: invalid_params }
+
+      it "should renders the 'edit' template" do
         expect(response).to render_template(:edit)
-        expect(flash[:error]).to include("Name can't be blank", "Screen type can't be blank", "Seating capacity can't be blank",
-        "Contact number can't be blank",
-        "Contact email can't be blank")
+      end
+
+      it 'should display error messages' do
+        expect(flash[:error]).to include("Name can't be blank")
+        expect(flash[:error]).to include("Screen type can't be blank")
+        expect(flash[:error]).to include("Seating capacity can't be blank")
+        expect(flash[:error]).to include("Contact number can't be blank")
+        expect(flash[:error]).to include("Contact email can't be blank")
       end
     end
   end
 
   describe "DELETE #destroy" do
-    it "destroys the requested theatre" do
-      theatre_to_delete = create(:theatre)
+    it "should destroys the requested theatre" do
       expect {
-        delete admin_theatre_path(theatre_to_delete)
+        delete admin_theatre_path(theatre)
       }.to change(Theatre, :count).by(-1)
       expect(flash[:notice]).to eq('Theatre was successfully destroyed.')
     end
 
-    it "redirects to the theatres list" do
+    it "should redirects to the theatres list with notice" do
       delete admin_theatre_path(theatre)
       expect(response).to redirect_to(admin_theatres_path)
       expect(flash[:notice]).to eq('Theatre was successfully destroyed.')
